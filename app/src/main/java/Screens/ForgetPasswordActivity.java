d723f42242bbe5ad83d83d504990fd3376a1d87b
AnsariFarah123 <168174857+AnsariFarah123@users.noreply.github.com>
2024-05-12 14:28:34 +0500

    ok

--- app/src/main/java/Screens/ForgetPasswordActivity.java
+++ app/src/main/java/Screens/ForgetPasswordActivity.java
@@ -1,6 +1,8 @@
 package Screens;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +19,11 @@ public class ForgetPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_forget_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+       getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+               WindowManager.LayoutParams.FLAG_FULLSCREEN);
+       findViewById(R.id.cancelBtn).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) { ForgetPasswordActivity.super.onBackPressed();}
+       });
     }
 }
