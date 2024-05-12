3158a8ad8d738b9c9d946a5cc394f9efefaec9da
AnsariFarah123 <168174857+AnsariFarah123@users.noreply.github.com>
2024-05-13 00:43:14 +0500

    ok

--- app/src/main/java/Screens/WelcomeActivity.java
+++ app/src/main/java/Screens/WelcomeActivity.java
@@ -1,13 +1,11 @@
 package Screens;
 
-<<<<<<< Updated upstream
+
 import android.os.Bundle;
-=======
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
->>>>>>> Stashed changes
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,13 +21,6 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-<<<<<<< Updated upstream
-        setContentView(R.layout.activity_welcome2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-=======
         setContentView(R.layout.activity_welcome);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -37,9 +28,10 @@ public class WelcomeActivity extends AppCompatActivity {
         findViewById(R.id.skipBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(WelcomeActivity.this,FinishActivity.class));
-            }
->>>>>>> Stashed changes
-        });
+                startActivity(new Intent(WelcomeActivity.this,FinishActivity));
+
     }
+
+    });
+}
 }
